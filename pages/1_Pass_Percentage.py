75f77afe5c8f6397ea46045060725efa8b90d605
Talha-Altair <cooltalha27@gmail.com>
2023-04-25 14:30:29 +0530

    Updates for 2023-04-25 14:30:29

--- pages/1_Pass_Percentage.py
+++ pages/1_Pass_Percentage.py
@@ -1,16 +1,29 @@
 import streamlit as st
 import pandas as pd
+from dotenv import load_dotenv
+import requests
+import os
+
+load_dotenv()
+
+BACKEND_URL = os.getenv("BACKEND_URL")
 
 st.set_page_config(page_title="Pass Percentage", page_icon="📊", initial_sidebar_state="expanded")
 
 year = st.multiselect("Filter Year", [1,2,3,4], default=[1,2,3,4])
 
-pass_data = pd.DataFrame({
-    'Subject': ['Compiler', 'Green Design', 'SPM', 'Operating System', 'Cloud Computing'],
-    'Pass Percentage': [76, 99, 97, 86, 82],
-    'year': [1,2,2,3,3],
-    'Subject Code': ['CS 201', 'CS 301', 'CS 401', 'CS 501', 'CS 601'],
-})
+response = requests.post(f"{BACKEND_URL}/teaching/passpercentage", json={"teacher_id": "3"}, headers={"Content-Type": "application/json"})
+
+pass_data = response.json()
+
+pass_data = pd.DataFrame(dict(pass_data))
+
+# pass_data = pd.DataFrame({
+#     'Subject': ['Compiler', 'Green Design', 'SPM', 'Operating System', 'Cloud Computing'],
+#     'Pass Percentage': [76, 99, 97, 86, 82],
+#     'year': [1,2,2,3,3],
+#     'Subject Code': ['CS3102', 'CS1024', 'CS2105', 'CS2106', 'CS3107'],
+# })
 
 pass_data = pass_data[pass_data['year'].isin(year)]
 
